cea28ffc33d93ee0c6ae81366097cd1b07a4b71a
Shweta Singh <shweta.singh@appliedtechnology.se>
2019-10-31 09:28:04 +0100

    search enter issue resolved

--- scripts/search.js
+++ scripts/search.js
@@ -1,5 +1,19 @@
 const searchBtn = document.querySelector('#searchButton');
 
+const getValueByEnter = searchBox => {
+  if (event.key === 'Enter') {
+    searchVal = searchBox.value;
+    console.log(searchBox.value);
+
+    fetch(`${search_url}?q=${searchVal}&token=${_api_key}`)
+      .then(res => res.json())
+      .then(data => {
+        handleFetch(data);
+      })
+      .catch(console.log);
+  }
+};
+
 searchBtn.addEventListener('click', e => {
   e.preventDefault();
   const searchVal = document.querySelector('#search').value;
